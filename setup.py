479d79285fb9d9d4b7d7b06adcb371caacc92cda
Taylor Dettling <taylor@binarynightowl.com>
2020-03-25 23:53:29 -0500

    update setup.py
    Signed-off-by: Taylor Dettling <taylor@binarynightowl.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 import setuptools
-
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -14,13 +13,19 @@ setup(
     description='A fast, powerful, and flexible way to get up to date COVID-19 data for any major city, state, '
                 'country, and total world wide data',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/binarynightowl/covid19_python',
     author='Taylor Dettling',
     author_email='taylor@binarynightowl.com',
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=[],
+    keywords='covid covid-19 corona coronavirus query hopkins python',
+    python_requires='>=3',
     classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ]
