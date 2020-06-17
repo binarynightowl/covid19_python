880f704ded4e7c7f5c3c5c2ae371780ad185b334
Taylor Dettling <taylor@binarynightowl.com>
2020-06-17 01:46:23 -0500

    Implement entities (state, country, world) as objects with the data treated as an attribute

--- covid19_data/JHU/__init__.py
+++ covid19_data/JHU/__init__.py
@@ -1,6 +1,6 @@
 from .get_data import get_all_data
 from .get_object import Item, Items
-from ..dict_as_attribute import DictAsObj as DictToObj
+from covid19_data.dict_as_attribute import DictAsObj as DictToOjb
 import inspect
 import re
 
@@ -35,7 +35,6 @@ states = {
     'MP': 'NorthernMarianaIslands',
     'MS': 'Mississippi',
     'MT': 'Montana',
-    'NA': 'National',
     'NC': 'NorthCarolina',
     'ND': 'NorthDakota',
     'NE': 'Nebraska',
@@ -120,3 +119,83 @@ def jsonByNameShort(name):
     item = Item(states[name].upper())
     item.rtrn_data()
     return item.json
+
+
+states = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'AmericanSamoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'DistrictofColumbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'NorthernMarianaIslands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NC': 'NorthCarolina',
+    'ND': 'NorthDakota',
+    'NE': 'Nebraska',
+    'NH': 'NewHampshire',
+    'NJ': 'NewJersey',
+    'NM': 'NewMexico',
+    'NV': 'Nevada',
+    'NY': 'NewYork',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'PuertoRico',
+    'RI': 'RhodeIsland',
+    'SC': 'SouthCarolina',
+    'SD': 'SouthDakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'VirginIslands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'WestVirginia',
+    'WY': 'Wyoming'
+}
+
+US_obj = dataByName("US")
+us_dict = {
+    "deaths": US_obj.deaths, "cases": US_obj.cases, "recovered": US_obj.recovered, "confirmed": US_obj.cases
+}
+
+total_obj = dataByName("Total")
+total_dict = {
+    "deaths": total_obj.deaths, "cases": total_obj.cases, "recovered": total_obj.recovered, "confirmed": total_obj.cases
+}
+
+for state in states:
+    name = states[state]
+    obj = dataByName(name)
+    data_dict = {"deaths": obj.deaths,
+                 "cases": obj.cases, "recovered": obj.recovered, "confirmed": obj.cases}
+    globals()[name] = DictToOjb(data_dict)
+
+US = DictToOjb(us_dict)
+Total = DictToOjb(total_dict)
--- covid19_data/__init__.py
+++ covid19_data/__init__.py
@@ -1,2 +1,3 @@
 from covid19_data.JHU import dataByCallerName, dataByCallerNameShort, dataByName, dataByNameShort, jsonByCallerName, \
     jsonByCallerNameShort, jsonByName, jsonByNameShort
+from covid19_data import JHU
