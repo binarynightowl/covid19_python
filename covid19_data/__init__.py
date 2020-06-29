5ac2427c10cba8fefd4948298ae5f0c891de70e2
Taylor Dettling <taylor@binarynightowl.com>
2020-06-29 15:28:55 -0500

    Add attribute style data retrieval for countries

--- covid19_data/__init__.py
+++ covid19_data/__init__.py
@@ -1,8 +1,3 @@
 from covid19_data.JHU import dataByCallerName, dataByCallerNameShort, dataByName, dataByNameShort, jsonByCallerName, \
     jsonByCallerNameShort, jsonByName, jsonByNameShort
 from covid19_data import JHU
-
-print("The current number of COVID-19 recoveries in the US according to John Hopkins is: " + JHU.US.recovered)
-print("The current number of confirmed COVID-19 cases in Texas according to John Hopkins is: " + JHU.Texas.confirmed)
-print("The current number of COVID-19 deaths in California according to John Hopkins is: " + JHU.California.deaths)
-print("The current number of worldwide COVID-19 deaths according to John Hopkins is: " + JHU.Total.deaths)
