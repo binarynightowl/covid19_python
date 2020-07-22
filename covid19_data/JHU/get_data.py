d04d5b752fd5e8f84fe82a01957c709c263776dc
Taylor Dettling <taylor@binarynightowl.com>
2020-07-21 19:00:49 -0500

    Patch errors that occurred in corner cases, and when a select few countries were called, as there were typos and/or special characters in the John Hopkins back end database, causing the API to fail over

--- covid19_data/JHU/get_data.py
+++ covid19_data/JHU/get_data.py
@@ -14,23 +14,26 @@ def get_data(url):
 
 
 def get_all_data(links_list):
-    remove_space = re.compile(r'\s+')
+    remove_space = re.compile('[^a-zA-Z]')
     for link in links_list:
         doc = get_data(link)
         for item in doc:
             if 'Country_Region' in item['attributes'].keys():
                 attributes = item['attributes']
                 country = re.sub(remove_space, '', attributes['Country_Region']).upper()
+                print(country)
                 attributes.pop('Country_Region', None)
                 document.update({country: attributes})
             elif 'Province_State' in item['attributes'].keys():
                 attributes = item['attributes']
                 stateprov = re.sub(remove_space, '', attributes['Province_State']).upper()
+                print(stateprov)
                 attributes.pop('Province_State', None)
                 document.update({stateprov: attributes})
             else:
                 attributes = item['attributes']
                 name = "Total".upper()
+                print(name)
                 document.update({name: attributes})
 
     return document
